7431dc9da9475c85589a75cf2776724efa74417f
VectorVP <vvtumko@mail.ru>
2021-06-20 08:14:52 +0300

    Change functionality to process html

--- processor/app.py
+++ processor/app.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 @app.route('/process', methods = ['POST'])
 def process_document():
-    image = request.data
-    image = preprocessor(image)
-    icd_list = parse_image(image)
-    return jsonify(icd_list)
+    data = request.data
+    data_processed = process_html(data, 60)
+    return jsonify(data_processed)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
